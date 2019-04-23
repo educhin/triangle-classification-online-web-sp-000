306e0686b71f384530ca977d909b1e14bc4d6cf9
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-23 02:09:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,19 @@ attr_accessor :side1, :side2, :side3
   end
 
   def kind
+    a = @side1
+    b = @side2
+    c = @side3
     valid = (a + b <= c || a + c <= b || b + c <= a)
+
+    if !valid
+      raise TriangleError
+    else
+
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
   end
 end
